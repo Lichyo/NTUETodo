18dd2ad15dde0624ceeb43151658a745b23ad1ce
Lichyo <chyoxuan@gmail.com>
2023-02-11 23:16:49 +0800

    Home Page Setup

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kAppBarTitleTextStyle = TextStyle(
+  fontWeight: FontWeight.w700,
+
+);
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:todo/constants.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  void onItemPressed(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'setting',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: onItemPressed,
+      ),
+      appBar: AppBar(
+        title: const Text('TODO',
+          style: kAppBarTitleTextStyle,
+        ),
+      ),
+    );
+  }
+}
